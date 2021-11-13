c074ae9472a75dc241da2cf4caa0721faacfa881
Derek Cross <derekcrosslu@gmail.com>
2021-11-12 19:08:13 -0500

    Update VideoFrame.tsx

--- src/components/VideoFrame.tsx
+++ src/components/VideoFrame.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./VideoFrame.module.css";
-
+// this is the only component that I tested besides the app component
+// because jest runner needs additional logic to do the "end to end" test and without it
+// some components will break for not being render below their parent component's and thus props appear as 
 function VideoFrame(props: any) {
     return (
       <main className={styles.main}>
